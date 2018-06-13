da89b21a73db489ff3ac16ff97b860092fa774d3
yuki9467 <1044081923@qq.com>
2018-06-13 13:17:47 +0800

    '提交数据管理系统eurekaserver'

---
+++ EurekaServer/src/main/java/com/bqhx/yyb/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.bqhx.yyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication 
+{
+    public static void main( String[] args ) throws Exception {
+    	SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
